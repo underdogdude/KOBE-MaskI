c582d5ad79650f6bce0879eb17ff9055aae1f5c9
underdogdude <underdogbright@gmail.com>
2021-03-30 22:06:11 +0800

    Before Johnny Five

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ var emoji_natural = document.getElementById("natural");
 var emotions_elems = document.getElementById("emotions");
 var main_emotions_elems = document.getElementById("main_emotion");
 
+
+// Record 
+var is_record = 0;
+var record_value = "";
 try {
     window.AppInventor.setWebViewString("js load5");
 } catch (err) {
@@ -24,7 +28,6 @@ var count = 0;
 // Sad and Angry
 let detectExpressions = async (result) => {
     // detect expression
-    console.log(result);
 
     if (typeof result !== "undefined") {
         let sad = 0,
@@ -46,32 +49,13 @@ let detectExpressions = async (result) => {
         var maxValue = Math.max.apply(null,Object.keys(result.expressions).map((o) => result.expressions[o]) );
         console.log(maxValue);
         var maxEmotion = Object.keys(result.expressions).find((o) => result.expressions[o] === maxValue); 
+        
+        if (is_record === 1) { 
+            record_value += maxEmotion + " (" + maxValue + ")" + "\n"; 
+        }
         var stringEmotion = `<img src="./img/emoji/${maxEmotion}.png" width="90%" /> <h1 class="text--capital m-color">${maxEmotion}</h1>`;
         $(main_emotions_elems).html(stringEmotion);
         $(emotions_elems).html(string);
-
-        // if (result.expressions.hasOwnProperty("sad")) {
-        //     sad = result.expressions.sad;
-        // }
-        // if (result.expressions.hasOwnProperty("angry")) {
-        //     anger = result.expressions.angry;
-        // }
-        // if (sad > 0.7) {
-        //     count += 1;
-        //     onExpression("sad");
-        //     if (count === 3) {
-        //         alert("you really sad");
-        //     }
-        // } else if (anger > 0.7) {
-        //     count += 1;
-        //     onExpression("angry");
-        //     if (count === 3) {
-        //         alert("you really angry");
-        //     }
-        // } else {
-        //     count = 0;
-        //     onExpression("natural");
-        // }
     }
 };
 
@@ -114,11 +98,29 @@ async function onPlay() {
         faceapi.draw.drawDetections(canvas, resizedResult)
         faceapi.draw.drawFaceExpressions(canvas, resizedResult, minConfidence)
     }
-    setTimeout(() => onPlay(), 2000);
+    setTimeout(() => onPlay(), 1000);
 }
 
 async function run() {
     const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
     const videoEl = $("#inputVideo").get(0);
     videoEl.srcObject = stream;
+}
+
+
+function record () { 
+
+    var elem = document.getElementById('record_btn');
+    if (is_record === 0) { 
+        console.log('bite');
+        elem.innerHTML = "&#9725;";
+        elem.classList.add("active");
+        is_record = 1
+    }else { 
+        elem.innerHTML = "Rec";
+        elem.classList.remove("active");
+        is_record = 0
+        console.log(record_value);
+        record_value ="";
+    }
 }
